8d019fd8100f0f7dbfd3f1711e0191aef08bc719
parnurzeal <parnurzeal@gmail.com>
2025-05-28 21:41:45 +0800

    Working prototype for mcp send/receive

--- main.py
+++ main.py
@@ -126,8 +126,10 @@ async def main_server_loop():
 
     # Wi-Fi Configuration (only used if RUN_WIFI_SERVER is True)
     # IMPORTANT: Replace with your actual Wi-Fi credentials if using Wi-Fi server
-    WIFI_SSID = "🌞AsaHome🏠"
-    WIFI_PASSWORD = "happywifehappylife"
+    # WIFI_SSID = "🌞AsaHome🏠"
+    # WIFI_PASSWORD = "happywifehappylife"
+    WIFI_SSID = "Pixel_A"
+    WIFI_PASSWORD = "94899604"
     MCP_SERVER_PORT = 8080  # Default port for Wi-Fi server, can be changed
 
     # Setup registries (common for both server types)
